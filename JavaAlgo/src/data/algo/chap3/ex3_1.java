df8e95e2e72270fc72984d15ca05350e6497768e
wogud556 <wogud556@naver.com>
2021-06-23 17:34:56 +0900

    2021-06-23 공부진행

---
+++ JavaAlgo/src/data/algo/chap3/ex3_1.java
@@ -0,0 +1,41 @@
+package data.algo.chap3;
+
+import java.util.Scanner;
+
+public class ex3_1 {
+    static int seqSearch(int[] a, int n, int key){
+        int i = 0;
+
+        while(true){
+            if(i == n)
+                return -1;
+            if(a[i]== key)
+                return i;
+            i++;
+        }
+    }
+
+    public static void ex31(){
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("요솟수 : ");
+        int num = stdIn.nextInt();
+        int[] x = new int[num];
+
+        for(int i = 0; i<num;i++){
+            System.out.println("x[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        System.out.println("검색할 값 : ");
+        int ky = stdIn.nextInt();
+
+        int idx = seqSearch(x, num, ky);
+
+        if(idx == -1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+
+    }
+}
