26fc28c6e03445e15fa3e00c33d42050ef957af1
wogud556 <wogud556@naver.com>
2021-06-28 13:07:38 +0900

    2021-06-28 공부진행

--- JavaAlgo/src/data/algo/chap3/ex3_1.java
+++ JavaAlgo/src/data/algo/chap3/ex3_1.java
@@ -1,5 +1,6 @@
 package data.algo.chap3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ex3_1 {
@@ -129,4 +130,39 @@ public class ex3_1 {
             System.out.println(ky+"은(는) x[" + idx + "]에 있습니다.");
 
     }
+
+    public static void ex3_5(){
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = stdIn.nextInt();
+        int[] x = new int[num];
+        
+        System.out.println("오름차순으로 입력하세요");
+
+        System.out.print("x[0] : ");
+        x[0] = stdIn.nextInt();
+
+        for(int i = 1; i< num; i++){
+            do{
+                System.out.println("x[" + i + "] :");
+                x[i] = stdIn.nextInt();
+            } while(x[i] < x[i-1]);
+        }
+
+        System.out.print("검색할 값 : ");
+        int ky = stdIn.nextInt();
+
+        int idx = Arrays.binarySearch(x,ky);
+
+        if(idx < 0){
+            System.out.println("그 값의 요소가 없습니다.");
+            //연습문제 8
+            System.out.println("삽입한 포인트의 값은" + ky + " 입니다.");
+        }else{
+            System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+        }
+
+    }
+
 }
---
+++ JavaAlgo/src/data/algo/chap3/pl3_1.java
@@ -0,0 +1,10 @@
+package data.algo.chap3;
+
+public class pl3_1 {
+
+    static int[] searchIdx(int[] a, int n, int key, int[] idx){
+
+        return new int[0];
+    }
+}
+
