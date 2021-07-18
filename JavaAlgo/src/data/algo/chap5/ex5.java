5320e1fabb6be30820681f3bb64d86764dc5c0fc
wogud556 <wogud556@naver.com>
2021-07-18 17:56:07 +0900

    20210718 공부진행

--- JavaAlgo/src/data/algo/chap5/ex5.java
+++ JavaAlgo/src/data/algo/chap5/ex5.java
@@ -18,4 +18,24 @@ public class ex5 {
 
         System.out.println(x + "의 팩토리얼은 " + factorial(x) + "이다.");
     }
+
+    static int gcd(int x, int y){
+        if(y == 0){
+            return x;
+        }else{
+            return gcd(y, x%y);
+        }
+    }
+
+    public static void ex5_2(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("두 정수의 최대공약수를 구합니다.");
+
+        System.out.println("정수를 입력하세요"); int x = stdIn.nextInt();
+        System.out.println("정수를 입력하세요"); int y = stdIn.nextInt();
+
+        System.out.println("최대공약수는 " + gcd(x, y) + "입니다.");
+
+    }
 }
